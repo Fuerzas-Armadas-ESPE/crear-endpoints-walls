66f6c64380990e5587b3fe31c17f9401e35ee964
Michelle Estefania Paredes Almachi <93295359+MichelleParedes@users.noreply.github.com>
2024-03-03 14:29:17 -0500

    Add files via upload

---
+++ src/src/app.module.ts
@@ -0,0 +1,11 @@
+//app.module.ts
+
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comment.module'; // Importa el módulo de comentarios
+
+@Module({
+  imports: [AuthModule, PostsModule, CommentsModule], // Importa también el módulo de comentarios
+})
+export class AppModule {}
